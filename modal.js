dc1708f5413e37fcb03fb4314b9859e424d06e8a
Ayash13 <m.ayashal.f@gmail.com>
2023-10-02 02:20:04 +0700

    fix bug

--- modal.js
+++ modal.js
@@ -1,34 +1,43 @@
-// Add click event listener to the "Aku sakit" button
-sakitButton.addEventListener('click', function () {
-    sendMessage('Ada orang sakit!!!!');
-    window.location.href = "./assets/Flash/sakit.gif";
-});
+document.addEventListener("DOMContentLoaded", function () {
+    const modal = document.getElementById('myModal');
+    const sakitButton = document.getElementById('sakitButton');
+    const daruratButton = document.getElementById('daruratButton');
 
-// Add click event listener to the "Aku ga baik-baik aja" button
-daruratButton.addEventListener('click', function () {
-    sendMessage('Keadaan darurat!!!!');
-    window.location.href = "./assets/Flash/darurat.gif";
-});
+    // Add click event listener to the "Aku sakit" button
+    sakitButton.addEventListener('click', function () {
+        sendMessage('Ada orang sakit!!!!');
+        window.location.href = "./assets/Flash/sakit.gif";
+    });
 
-function sendMessage(messageContent) {
-    // Replace 'your-formcarry-endpoint' with your actual Formcarry endpoint
-    const formcarryEndpoint = 'https://formcarry.com/s/LxVEbStyS';
+    // Add click event listener to the "Aku ga baik-baik aja" button
+    daruratButton.addEventListener('click', function () {
+        sendMessage('Keadaan darurat!!!!');
+        window.location.href = "./assets/Flash/darurat.gif";
+    });
 
-    // You can send the message here using an AJAX request to Formcarry
-    fetch(formcarryEndpoint, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ message: messageContent }), // Use the provided message content
-    })
-        .then(response => {
-            // Handle the response as needed
-            // For example, you can show a confirmation message to the user
-            console.log('Message sent successfully');
+    function sendMessage(messageContent) {
+        // Replace 'your-formcarry-endpoint' with your actual Formcarry endpoint
+        const formcarryEndpoint = 'https://formcarry.com/s/LxVEbStyS';
+
+        // You can send the message here using an AJAX request to Formcarry
+        fetch(formcarryEndpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ message: messageContent }), // Use the provided message content
         })
-        .catch(error => {
-            // Handle any errors that occur during the request
-            console.error('Error sending message:', error);
-        });
-}
+            .then(response => {
+                // Handle the response as needed
+                // For example, you can show a confirmation message to the user
+                console.log('Message sent successfully');
+            })
+            .catch(error => {
+                // Handle any errors that occur during the request
+                console.error('Error sending message:', error);
+            });
+
+        // Close the modal after sending the message
+        modal.style.display = 'none';
+    }
+});
