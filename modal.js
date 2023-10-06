fbac772f9994a5b7cd1c12249da14e375c88637c
Ayash13 <m.ayashal.f@gmail.com>
2023-10-06 09:43:51 +0700

    refactor code

--- modal.js
+++ modal.js
@@ -2,6 +2,31 @@ document.addEventListener("DOMContentLoaded", function () {
     const modal = document.getElementById('myModal');
     const sakitButton = document.getElementById('sakitButton');
     const daruratButton = document.getElementById('daruratButton');
+    const formcarryEndpoint = 'https://formcarry.com/s/j7pCRt6VKb';
+
+    // Function to send a message to Formcarry
+    function sendMessage(messageContent) {
+        fetch(formcarryEndpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ message: messageContent }),
+        })
+        .then(response => {
+            if (response.ok) {
+                console.log('Message sent successfully');
+            } else {
+                console.error('Error sending message:', response.status);
+            }
+        })
+        .catch(error => {
+            console.error('Error sending message:', error);
+        })
+        .finally(() => {
+            modal.style.display = 'none'; // Close the modal after sending the message
+        });
+    }
 
     // Add click event listener to the "Aku sakit" button
     sakitButton.addEventListener('click', function () {
@@ -14,30 +39,4 @@ document.addEventListener("DOMContentLoaded", function () {
         sendMessage('Keadaan darurat!!!!');
         window.location.href = "./assets/Flash/darurat.gif";
     });
-
-    function sendMessage(messageContent) {
-        // Replace 'your-formcarry-endpoint' with your actual Formcarry endpoint
-        const formcarryEndpoint = 'https://formcarry.com/s/j7pCRt6VKb';
-
-        // You can send the message here using an AJAX request to Formcarry
-        fetch(formcarryEndpoint, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ message: messageContent }), // Use the provided message content
-        })
-            .then(response => {
-                // Handle the response as needed
-                // For example, you can show a confirmation message to the user
-                console.log('Message sent successfully');
-            })
-            .catch(error => {
-                // Handle any errors that occur during the request
-                console.error('Error sending message:', error);
-            });
-
-        // Close the modal after sending the message
-        modal.style.display = 'none';
-    }
 });
